684013d24fbd9566f474c711d6c2b028d09fbb85
azeng68 <azeng68@gmail.com>
2016-06-18 10:55:00 -0400

    Update ServoTest.java
    Added telemetry for easier servo debugging and various comments

--- AYS Outreach 2016/ServoTest.java
+++ AYS Outreach 2016/ServoTest.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.Range;
 /**
  * Created by citruseel on 6/10/2016.
  */
-public class ServoTest extends LinearOpMode {
+public class ServoTestPrgm extends LinearOpMode {
 
     private Servo testing;
 
@@ -17,12 +17,18 @@ public class ServoTest extends LinearOpMode {
         ServoMove();
         ServoStart();
     }
+    
+    //Sets servo position to 0
     public void ServoStart()
     {
         testing.setPosition(0);
+        telemetry.addData("ServoTest", "Servo: " + testing);
     }
+    
+    //Sets servo position to 0.5
     public void ServoMove()
     {
         testing.setPosition(0.5);
+        telemetry.addData("ServoTest", "Servo: " + testing);
     }
 }
